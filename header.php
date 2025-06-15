5f5778f4eff85044b8047771501b61361ea2d786
junpisa <junpisa@gmail.com>
2025-06-15 11:39:26 +0700

    แก้ไขเพิ่มเติม ส่วนของรูปภาพโปรไฟล์

--- header.php
+++ header.php
@@ -25,6 +25,7 @@ header('Content-Type: text/html; charset=utf-8');
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/css/profile-dashboard.css" rel="stylesheet">
 
   <!-- Main CSS File -->
   <link href="assets/css/main.css" rel="stylesheet">
