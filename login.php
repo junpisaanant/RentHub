d81317be2e153daa76c8cb85353726c49cdda29a
junpisa <junpisa@gmail.com>
2025-09-21 11:50:30 +0700

    รองรับการเปลี่ยนภาษา

--- login.php
+++ login.php
@@ -1,49 +1,47 @@
+<?php
+session_start();
+
+if (isset($_GET['lang'])) {
+    $_SESSION['lang'] = $_GET['lang'];
+} elseif (!isset($_SESSION['lang'])) {
+    $_SESSION['lang'] = 'th';
+}
+
+$lang_file = 'languages/' . $_SESSION['lang'] . '.php';
+if (file_exists($lang_file)) {
+    include $lang_file;
+} else {
+    include 'languages/th.php';
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php echo $_SESSION['lang']; ?>">
 <head>
-	<title>Login V5</title>
+	<title><?php echo $lang['login_title']; ?></title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
 <script src="login/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="login/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="login/vendor/bootstrap/js/popper.js"></script>
+<script src="login/vendor/animsition/js/animsition.min.js"></script>
+<script src="login/vendor/bootstrap/js/popper.js"></script>
 	<script src="login/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="login/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="login/vendor/daterangepicker/moment.min.js"></script>
+<script src="login/vendor/select2/select2.min.js"></script>
+<script src="login/vendor/daterangepicker/moment.min.js"></script>
 	<script src="login/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="login/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="login/js/main.js"></script>
+<script src="login/vendor/countdowntime/countdowntime.js"></script>
+<script src="login/js/main.js"></script>
 	<script src="https://apis.google.com/js/platform.js" async defer></script>
 	<script src="login/js/login.js"></script>
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="assets/img/favicon.png"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="login/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="login/css/util.css">
+<link rel="icon" type="image/png" href="assets/img/favicon.png"/>
+<link rel="stylesheet" type="text/css" href="login/vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<link rel="stylesheet" type="text/css" href="login/vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="login/vendor/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css" href="login/vendor/animsition/css/animsition.min.css">
+<link rel="stylesheet" type="text/css" href="login/vendor/select2/select2.min.css">
+<link rel="stylesheet" type="text/css" href="login/vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="login/css/util.css">
 	<link rel="stylesheet" type="text/css" href="login/css/main.css">
-<!--===============================================================================================-->
 </head>
 <?php 
 $mode = 'login';
@@ -54,51 +52,40 @@ if (isset($user_id)) {
     exit();
 }
 ?>
+<body>
+	<div style="position: absolute; top: 10px; right: 10px; z-index: 1000;">
+		<a href="?lang=th" style="text-decoration: none; color: white; margin-right: 10px;">TH</a>
+		<a href="?lang=en" style="text-decoration: none; color: white; margin-right: 10px;">EN</a>
+		<a href="?lang=cn" style="text-decoration: none; color: white;">CN</a>
+	</div>
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('assets/img/theprestige-2.png');">
 			<div class="wrap-login100 p-l-110 p-r-110 p-t-62 p-b-33">
 				<form class="login100-form validate-form flex-sb flex-w" method="post">
 					<span class="login100-form-title p-b-53">
-						ลงชื่อเข้าใช้
+						<?php echo $lang['sign_in']; ?>
 					</span>
-					<!--
-						เดี๋ยวมาตามเก็บทีหลังตอนมีเวลาเหลือ
-					<a href="#" class="btn-face m-b-20">
-						<i class="fa fa-facebook-official"></i>
-						Facebook
-					</a>
-					
-						ปุ่มเดิมสวยกว่า
-						<a href="#" class="btn-google m-b-20" data-onsuccess="onSignIn">
-							<img src="login/images/icons/icon-google.png" alt="GOOGLE">
-							Google
-						</a>	
-					<meta name="google-signin-client_id" content="976930577576-0s9uacn3q8u5dsd1ne76s1lm32lpclj6.apps.googleusercontent.com">
-						<div class="g-signin2" data-onsuccess="onSignIn"></div>
-					</meta>	-->
-					
-					
 					
 					<div class="p-b-9">
 						<span class="txt1">
-							Username
+							<?php echo $lang['username']; ?>
 						</span>
 					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Username is required">
+					<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['username_is_required']; ?>">
 						<input class="input100" type="text" name="username" autocomplete="username">
 						<span class="focus-input100"></span>
 					</div>
 					
 					<div class="p-t-13 p-b-9">
 						<span class="txt1">
-							Password
+							<?php echo $lang['password']; ?>
 						</span>
 
-						<a href="#" class="txt2 bo1 m-l-5">
-							ลืมรหัสผ่าน?
+						<a href="#" id="forgotPasswordLink" class="txt2 bo1 m-l-5">
+							<?php echo $lang['forgot_password_q']; ?>
 						</a>
 					</div>
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['password_is_required']; ?>">
 						<input class="input100" type="password" name="password"  autocomplete="current-password">
 						<span class="focus-input100"></span>
 						<span class="btn-show-pass">
@@ -108,17 +95,17 @@ if (isset($user_id)) {
 
 					<div class="container-login100-form-btn m-t-17">
 						<button class="login100-form-btn" id="loginBtn">
-							เข้าสู่ระบบ
+							<?php echo $lang['login_button']; ?>
 						</button>
 					</div>
 
 					<div class="w-full text-center p-t-55">
 						<span class="txt2">
-							ยังไม่ได้เป็นสมาชิก?
+							<?php echo $lang['not_a_member_q']; ?>
 						</span>
 
-						<a href="#" class="txt2 bo1">
-							สมัครสมาชิก
+						<a href="#" id="registerLink" class="txt2 bo1">
+							<?php echo $lang['sign_up_now']; ?>
 						</a>
 					</div>
 				</form>
@@ -126,149 +113,147 @@ if (isset($user_id)) {
 		</div>
 	</div>
 	
-	<!--ลืมรหัสผ่าน -->
 	<div id="forgotPasswordDialog" class="modal">
 		<div class="modal-content">
 			<span class="close-button">&times;</span>
-			<h2>ลืมรหัสผ่าน</h2>
+			<h2><?php echo $lang['forgot_password']; ?></h2>
 			<div class="p-b-9">
 				<span class="txt1">
-					อีเมล
+					<?php echo $lang['email']; ?>
 				</span>
 			</div>
-			<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+			<div class="wrap-input100 validate-input" data-validate="<?php echo $lang['valid_email_is_required']; ?>">
 				<input class="input100" type="text" name="forgot_email">
 				<span class="focus-input100"></span>
 			</div>
 			<div class="container-login100-form-btn m-t-17">
 				<button class="login100-form-btn" id="forgotPasswordSubmit">
-					ส่งรหัสผ่านใหม่
+					<?php echo $lang['send_new_password']; ?>
 				</button>
 			</div>
 		</div>
 		
 	</div>
 
-	<!-- สมัครสมาชิก -->
 	<div id="registerDialog" class="modal">
-    <div class="modal-content-register">
-        <span class="close-button">&times;</span>
-        <h2>สมัครสมาชิก</h2>
-		<form id="registerForm"> 
-			<div class="p-b-9">
-				<span class="txt1">
-					Username (จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100 validate-input" data-validate = "Username is required">
-				<input class="input100" type="text" name="reg_username" minlength="8" autocomplete="username">
-				<span class="focus-input100"></span>
-			</div>
+		<div class="modal-content-register">
+			<span class="close-button">&times;</span>
+			<h2><?php echo $lang['register']; ?></h2>
+			<form id="registerForm"> 
+				<div class="p-b-9">
+					<span class="txt1">
+						<?php echo $lang['username_required']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['username_is_required']; ?>">
+					<input class="input100" type="text" name="reg_username" minlength="8" autocomplete="username">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					Password (จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100 validate-input" data-validate = "Password is required">
-				<input class="input100" type="password" name="reg_password" id="reg_password" autocomplete="current-password" pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required>
-				<span class="focus-input100"></span>
-				<span class="btn-show-pass">
-					<i class="fa fa-eye"></i>
-				</span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['password_required']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['password_is_required']; ?>">
+					<input class="input100" type="password" name="reg_password" id="reg_password" autocomplete="current-password" pattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$" required>
+					<span class="focus-input100"></span>
+					<span class="btn-show-pass">
+						<i class="fa fa-eye"></i>
+					</span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					ยืนยัน Password (จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100 validate-input" data-validate = "Password confirmation is required">
-				<input class="input100" type="password" name="reg_confirm_password" autocomplete="current-password" id="reg_confirm_password" required>
-				<span class="focus-input100"></span>
-				<span class="btn-show-pass">
-					<i class="fa fa-eye"></i>
-				</span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['confirm_password_required']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['password_confirmation_is_required']; ?>">
+					<input class="input100" type="password" name="reg_confirm_password" autocomplete="current-password" id="reg_confirm_password" required>
+					<span class="focus-input100"></span>
+					<span class="btn-show-pass">
+						<i class="fa fa-eye"></i>
+					</span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					Email (จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-				<input class="input100" type="text" name="reg_email">
-				<span class="focus-input100"></span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['email_required']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['valid_email_is_required']; ?>">
+					<input class="input100" type="text" name="reg_email">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					เบอร์โทรศัพท์ (จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100 validate-input" data-validate = "Phone number is required">
-				<input class="input100" type="text" name="reg_phone_no">
-				<span class="focus-input100"></span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['phone_number_required']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['phone_number_is_required']; ?>">
+					<input class="input100" type="text" name="reg_phone_no">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					Line ID (ไม่จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100">
-				<input class="input100" type="text" name="reg_line_id">
-				<span class="focus-input100"></span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['line_id_optional']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100">
+					<input class="input100" type="text" name="reg_line_id">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					ชื่อจริง (จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100 validate-input" data-validate = "Firstname is required">
-				<input class="input100" type="text" name="reg_firstname">
-				<span class="focus-input100"></span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['firstname_required']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['firstname_is_required']; ?>">
+					<input class="input100" type="text" name="reg_firstname">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					นามสกุล (จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100 validate-input" data-validate = "Lastname is required">
-				<input class="input100" type="text" name="reg_lastname">
-				<span class="focus-input100"></span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['lastname_required']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "<?php echo $lang['lastname_is_required']; ?>">
+					<input class="input100" type="text" name="reg_lastname">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					เลขบัตรประชาชน (ไม่จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100">
-				<input class="input100" type="text" name="reg_identification_no">
-				<span class="focus-input100"></span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['identification_no_optional']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100">
+					<input class="input100" type="text" name="reg_identification_no">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="p-t-13 p-b-9">
-				<span class="txt1">
-					Passport (ไม่จำเป็น)
-				</span>
-			</div>
-			<div class="wrap-input100">
-				<input class="input100" type="text" name="reg_passport_no">
-				<span class="focus-input100"></span>
-			</div>
+				<div class="p-t-13 p-b-9">
+					<span class="txt1">
+						<?php echo $lang['passport_no_optional']; ?>
+					</span>
+				</div>
+				<div class="wrap-input100">
+					<input class="input100" type="text" name="reg_passport_no">
+					<span class="focus-input100"></span>
+				</div>
 
-			<div class="container-login100-form-btn m-t-17">
-				<button class="login100-form-btn" id="registerSubmit">
-					ตกลง
-				</button>
-			</div>
-		</form>
-    </div>
-</div>
+				<div class="container-login100-form-btn m-t-17">
+					<button class="login100-form-btn" id="registerSubmit">
+						<?php echo $lang['ok']; ?>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
 
 </body>
 </html>
